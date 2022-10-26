02f39e50e385822bc76ea9094c4fa02a1369eeb8
francopautasso1 <fpautasso@gmail.com>
2022-10-26 00:47:28 -0300

    clase 1

---
+++ clase 01/app.js
@@ -0,0 +1,84 @@
+
+//clase sintaxis y variables
+
+//declaracion de variables
+let nombre = "Franco Pautasso"
+
+//console.log(nombre)
+
+let apellido
+let rol
+
+
+//asigno valor
+nombre = "Franco"
+edad = 31
+rol = "desarrolador"
+
+
+//console.log(nombre)
+//console.log(edad)
+//console.log(rol)
+
+//const anionacimiento = 1991
+
+
+//console.log(anionacimiento)
+
+
+//operaciones basicas
+
+let num1 = 10
+let num2 = 20
+let resultado = (num2 + num1) * 4
+
+//console.log(resultado)
+
+
+let text1 = "hola"
+let text2 = "mundo"
+
+let textoFinal = text1 + " " + text2 + "!!!"
+
+//console.log( textoFinal )
+//alert( textoFinal )
+
+//console.log( text1 + num2)
+//console.log(text1 + resultado)
+
+//let tutor = prompt("ingrese el tutor")
+
+//console.log( tutor  )
+
+
+
+const anioActual = 2022
+
+//let nombreCompleto = prompt("inrese su nombre completo")
+//let anionacimiento = prompt("ingrese su año de nacimiento")
+
+//let mensajeSalida = "Bienvenido" + " " + nombreCompleto + ". TU edad es :" + (anioActual - anionacimiento) + " " + "años"
+
+//console.log(nombreCompleto)
+//console.log(anionacimiento)
+//console.log(anioActual)
+
+//alert( mensajeSalida)
+
+//let num4 = prompt("ingrese un numero")
+//let num5 = prompt("ingrese un numero")
+
+//num4 = parseInt( num4 )
+//num5 = parseInt( num5 ) 
+
+//Number es lo mismo que parseINt
+
+//num4 = Number( num4 )
+//num5 = Number( num5 )
+
+let num4 = Number( prompt("ingrese un numero"))
+let num5 = Number( prompt("ingrese un numero"))
+
+
+
+console.log(num4 + num5)
